e3b8b02e0b21eccc3c92335b1b24abb0c31cf2f8
MonzerratBanuelos <ambgmonze@gmail.com>
2022-05-16 15:45:29 -0500

    adding eslint to  the project

---
+++ burger-queen/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}

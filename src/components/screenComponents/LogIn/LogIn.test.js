11fb50e5d2bd53c557e14068e0b65e3ce7d1a81d
Ziomara Jiménez <97549552+ziomarajimenez@users.noreply.github.com>
2022-05-16 20:57:50 -0600

    Adds test for error message in the login page

--- src/components/screenComponents/LogIn/LogIn.test.js
+++ src/components/screenComponents/LogIn/LogIn.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, act } from "@testing-library/react";
+import { fireEvent, render, screen, act, waitFor } from "@testing-library/react";
 import { LogIn } from './LogIn';
 
 const signInMock = jest.fn();
@@ -15,34 +15,32 @@ describe('LogIn component renders correctly', () => {
 
     it('Renders button', () => {
         render(<LogIn signInWithEmail={signInMock}></LogIn>);
-        screen.getByRole('button', {type: 'submit'});
+        screen.getByRole('button', { type: 'submit' });
         screen.getAllByText('Sign In');
     });
 
     it('Calls the sign in function when submitted', () => {
         render(<LogIn signInWithEmail={signInMock}></LogIn>);
         signInMock.mockResolvedValue('response');
-        const button = screen.getByRole('button', {type: 'submit'});
+        const button = screen.getByRole('button', { type: 'submit' });
         fireEvent.click(button);
 
         expect(signInMock).toHaveBeenCalled();
     });
 
-    it('Calls the sign in function when submitted and returns and error', () => {
-        const setErrorCode = jest.fn();
-        render(<LogIn signInWithEmail={signInMock}/>);
-        
+    it('Returns error message when submitted', async () => {
+        render(<LogIn signInWithEmail={signInMock} />);
+
         const error = {
-            code : 'auth/user-not-found',
+            code: 'auth/user-not-found',
         }
         signInMock.mockRejectedValue(error);
-        const button = screen.getByRole('button', {type: 'submit'});
 
+        const button = screen.getByRole('button', { type: 'submit' });
         fireEvent.click(button);
-        
-        //screen.debug();
-        expect(signInMock).toHaveBeenCalled();
-        //screen.getByText('This account does not exist.');
-    });
-})
 
+        await waitFor(() => {
+            screen.getByText('This account does not exist.');
+        });
+    })
+})

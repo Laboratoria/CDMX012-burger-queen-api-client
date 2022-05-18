295f3e12cd2f4b2de90dd6825753dd614ee2dd21
Ziomara Jiménez <97549552+ziomarajimenez@users.noreply.github.com>
2022-05-17 18:16:53 -0600

    responsive design for login page

--- src/components/screenComponents/LogIn/LogIn.js
+++ src/components/screenComponents/LogIn/LogIn.js
@@ -1,13 +1,13 @@
 import React from "react";
 import './LogIn.css';
-import logo from '../../../assets/burger queen logo fondo blanco.png';
+import logo from '../../../assets/logo grande fondo blanco.png';
 import leftBurger from '../../../assets/side-burger-left.png';
 import rightBurger from '../../../assets/side-burger-right.png';
 import { Footer } from '../../staticComponents/footer';
 import { LoginError } from "../../../utils/errorMessage";
 
 
-export const LogIn = ({signInWithEmail}) => {
+export const LogIn = ({ signInWithEmail }) => {
 
     const [errorCode, setErrorCode] = React.useState("");
 
@@ -31,12 +31,12 @@ export const LogIn = ({signInWithEmail}) => {
     const handleSubmit = (evt) => {
         evt.preventDefault();
         signInWithEmail(values.email, values.password)
-        .then((response) => {
-            console.log(response);
-        })
-        .catch((error) => {
-            setErrorCode(error.code);
-        })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                setErrorCode(error.code);
+            })
     }
 
     return (
@@ -48,23 +48,23 @@ export const LogIn = ({signInWithEmail}) => {
 
                 <label htmlFor='email' id="emailLabel">E-mail</label>
 
-                <input 
-                    type='text' 
-                    id='email' 
-                    name='email' 
-                    placeholder='myemail@gmail.com' 
+                <input
+                    type='text'
+                    id='email'
+                    name='email'
+                    placeholder='myemail@gmail.com'
                     value={values.email}
                     onChange={handleChange}>
                 </input>
 
                 <br></br>
-                    
+
                 <label htmlFor='password' id="passLabel">Password</label>
 
-                <input 
-                    type='password' 
-                    id='password' 
-                    name='password' 
+                <input
+                    type='password'
+                    id='password'
+                    name='password'
                     placeholder='mypassword123'
                     value={values.password}
                     onChange={handleChange}>
@@ -77,8 +77,9 @@ export const LogIn = ({signInWithEmail}) => {
 
                 <button type='submit'>Sign In</button>
             </form>
+            <Footer />
             <img src={leftBurger} alt="left side burger" className="left-burger burger"></img>
-            <Footer/>
+
 
         </div>
     );

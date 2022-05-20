59e36d24432b27f6aaca30f51b42c87622e1486d
Ziomara Jiménez <97549552+ziomarajimenez@users.noreply.github.com>
2022-05-20 11:30:06 -0600

    changes to router

--- src/lib/firebaseAuth.js
+++ src/lib/firebaseAuth.js
@@ -1,21 +1,21 @@
-import { 
-    createUserWithEmailAndPassword, 
-    getAuth, 
-    signOut, 
-    signInWithEmailAndPassword 
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    signOut,
+    signInWithEmailAndPassword
 } from "firebase/auth";
 import { auth } from './firebaseConfig';
 
 export const createAccWithEmail = (email, password) => {
     createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log(user);
-    })
-    .catch((error) => {
-        console.log(error)
-    });
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            console.log(user);
+        })
+        .catch((error) => {
+            console.log(error)
+        });
 }
 
 export const signInWithEmail = async (email, password) => {
@@ -25,9 +25,9 @@ export const signInWithEmail = async (email, password) => {
 export const logOut = () => {
     const auth = getAuth();
     signOut(auth).then(() => {
-    // Sign-out successful.
+        // Sign-out successful.
     }).catch((error) => {
-    // An error happened.
+        // An error happened.
     });
 }
 
--- src/routers/PublicRoutes.js
+++ src/routers/PublicRoutes.js
@@ -1,15 +1,20 @@
 import {
-    Routes,
-    Route,
-  } from "react-router-dom";
+  Routes,
+  Route,
+} from "react-router-dom";
 import { LogIn } from '../pages/LogIn/LogIn';
 import { signInWithEmail } from "../lib/firebaseAuth";
+import { Navigate } from "react-router-dom";
 
 
 export const PublicRoutes = () => {
-  return(
-      <Routes>
-          <Route path="/" element={<LogIn signInWithEmail={signInWithEmail} />} />
-      </Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<LogIn signInWithEmail={signInWithEmail} />} />
+      <Route
+        path="*"
+        element={<Navigate to="/" replace />}
+      />
+    </Routes>
   );
 }

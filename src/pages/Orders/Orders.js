90349fb3725f68dfbcf14e366c5f2f5ddb22aa84
maisahr <ma.isahr7@gmail.com>
2022-05-19 21:33:15 -0700

    Renders breakfast and dinner menu, separates beverages and meals

--- src/pages/Orders/Orders.js
+++ src/pages/Orders/Orders.js
@@ -3,6 +3,8 @@ import { Header } from "../../components/header";
 import { Footer } from "../../components/footer";
 import { useState, useEffect } from "react";
 import './Orders.css'
+import beverageIcon from '../../assets/beverage.png'
+import mealIcon from '../../assets/meal.png'
 export const Orders = () => {
 
     /* const dinnerMenu = data.products.filter(product => {return product.menu === 'dinner'}); */
@@ -23,32 +25,51 @@ export const Orders = () => {
 
     const breakfastMenu = () => {
         const breakfast = products.filter(product => { return product.menu === 'breakfast' });
+        const beverages = breakfast.filter(product => { return product.type === 'beverage' });
+        const meal = breakfast.filter(product => { return product.type === 'meal' });
+
         return (
-            <>
+            <div className="breakfastMenu">
                 <style>{`
                     .dinnerBtn {
                         background-color: white;
                         color: #B5A8A8;
                     }
                 `}</style>
-                {breakfast.map(products => <ProductBox product={products.name} key={products._id}></ProductBox>)}
-            </>
+                <div className='beverages'>
+                    <img src={beverageIcon} alt="beverage icon" className="beverageIcon"></img>
+                    {beverages.map(products => <ProductBox product={products.name} key={products._id}></ProductBox>)}
+                </div>
+                <div className='meal'>
+                    <img src={mealIcon} alt="meal icon" className="mealIcon"></img>
+                    {meal.map(products => <ProductBox product={products.name} key={products._id}></ProductBox>)}
+                </div>
+            </div>
         );
     }
 
     const dinnerMenu = () => {
-        const dinner = products.filter(product => { return product._id === 1 });
+        const dinner = products.filter(product => { return product.menu === 'dinner' });
+        const beverages = dinner.filter(product => { return product.type === 'beverage' });
+        const meal = dinner.filter(product => { return product.type === 'burger' || product.type === 'side dish'});
+
         return (
-            <>
+            <div className="dinnerMenu">
                 <style>{`
                     .breakfastBtn {
                         background-color: white;
                         color: #B5A8A8;
                     }
                 `}</style>
-                {dinner.map(products => <ProductBox product={products.name} key={products._id}></ProductBox>)}
-            </>
-
+                <div className='beverages'>
+                    <img src={beverageIcon} alt="beverage icon" className="beverageIcon"></img>
+                    {beverages.map(products => <ProductBox product={products.name} key={products._id}></ProductBox>)}
+                </div>
+                <div className='meal'>
+                    <img src={mealIcon} alt="meal icon" className="mealIcon"></img>
+                    {meal.map(products => <ProductBox product={products.name} key={products._id}></ProductBox>)}
+                </div>
+            </div>
         );
     }
 

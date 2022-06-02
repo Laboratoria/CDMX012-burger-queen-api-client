01fb623257089445dc062f84d1e769ac02fb4c30
Atenas Villalba <atenasvillalba8@gmail.com>
2022-06-01 23:23:44 -0500

    install mui-datatables

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import Products from "./Products"
 
 const Personal = () => {
   return (
     <div>
       <Header/>
       <h1>Vista de Administrador</h1>
+      <Products/>
     </div>
   );
 };

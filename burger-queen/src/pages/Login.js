8ddf5b9bce1dddc018d5ec3610db2489d4a8ed4b
AnaLizarraga <analiz.laboratoria@gmail.com>
2022-06-03 02:02:32 -0500

    HU1: alerts: invalid user, wrong mail and password

--- burger-queen/src/pages/Login.js
+++ burger-queen/src/pages/Login.js
@@ -1,17 +1,52 @@
 import '../styles/Login.css'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../Lib/firebase-keys'
 import bannerLoginHamb from '../Assets/Images/bannerLogIn.png'
 import LogoBQ from '../Assets/Images/BQWhite.png'
 
 export default function HomePage() {
+  const [errorEmail, setErrorEmail] = useState(false)
+  const [errorPassword, setErrorPassword] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
   const handleLogin = async () => {
     await signInWithEmailAndPassword(auth, email, password)
-    console.log(auth.currentUser)
+      .then((userCredential) => {
+        useNavigate('/')
+      })
+      .catch((error) => {
+        console.log(error.code)
+        switch (error.code) {
+          case 'auth/user-not-found':
+            return (
+              setErrorMessage('Ingresa un email válido'),
+              setErrorEmail('message_active')
+            )
+          case 'auth/wrong-password':
+            return (
+              setErrorMessage('Ingresa la contraseña correcta'),
+              setErrorPassword('message_active')
+            )
+          case 'auth/invalid-email':
+            return (
+              setErrorMessage('Ingresa un email y contraseña válidos'),
+              setErrorEmail('message_active'),
+              setErrorPassword('message_active')
+            )
+          case 'auth/internal-error':
+            return (
+              setErrorMessage('Ingresa un email y contraseña válidos'),
+              setErrorEmail('message_active'),
+              setErrorPassword('message_active')
+            )
+          default:
+            return setErrorMessage(false)
+        }
+      })
   }
 
   return (
@@ -23,7 +58,8 @@ export default function HomePage() {
       <aside className='right_aside_content'>
         <img src={LogoBQ} alt='Logo' className='Logo' />
         <section className='form_log_in'>
-          <input className='imput_log_in'
+          <input
+            className={`imput_log_in  ${errorEmail} `}
             type='text'
             id='login-email'
             placeholder='Empleado e-mail'
@@ -31,15 +67,27 @@ export default function HomePage() {
               setEmail(e.target.value)
             }}
           />
-          <input className='imput_log_in'
+
+          { errorMessage === 'Ingresa un email válido'
+            ? <div className='Login_error'>{errorMessage}</div>
+            : <div className='Login_error'></div>
+          }
+
+          <input
+            className={`imput_log_in  ${errorPassword} `}
             type='text'
-            id='id="login-password"'
+            id='login-password'
             placeholder='Contraseña'
             onChange={(e) => {
               setPassword(e.target.value)
             }}
           />
-          <button className='btn_log_in'
+          { errorMessage === 'Ingresa la contraseña correcta' || errorMessage === 'Ingresa un email y contraseña válidos'
+            ? <div className='Login_error'>{errorMessage}</div>
+            : <div className='Login_error'></div>
+          }
+          <button
+            className='btn_log_in'
             id='login-btn'
             onClick={() => {
               handleLogin()

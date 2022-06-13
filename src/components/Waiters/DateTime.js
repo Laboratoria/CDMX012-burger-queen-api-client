1c936231b88ade6a2af511e881f6a3c3258952c2
Atenas Villalba <atenasvillalba8@gmail.com>
2022-06-13 13:49:16 -0500

    changes in orders

---
+++ src/components/Waiters/DateTime.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+export default function DateTime() {
+  let [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => setDate(new Date()), 1000);
+    return function cleanup() {
+      clearInterval(timer);
+    };
+  });
+
+  return (
+    <div>
+    
+        {date.toDateString()} {date.toLocaleTimeString()}
+      
+    </div>
+  );
+}

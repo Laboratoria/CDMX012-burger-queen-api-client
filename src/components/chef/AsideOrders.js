d8b87ccc4c793537646694582520dc61c125147f
Atenas Villalba <atenasvillalba8@gmail.com>
2022-06-21 13:01:26 -0500

    showing cards with style

---
+++ src/components/chef/AsideOrders.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const AsideOrders = props => {
+  return (
+    <div>AsideOrders</div>
+  )
+}
+
+AsideOrders.propTypes = {}
+
+export default AsideOrders

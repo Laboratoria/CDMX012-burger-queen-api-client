1e51e22785ef6bce92b98b415ebb6241c403c613
Atenas Villalba <atenasvillalba8@gmail.com>
2022-06-26 22:31:24 -0500

    play with views if the user do click in buttons on header

--- src/components/Waiters/MenuView.js
+++ src/components/Waiters/MenuView.js
@@ -1,150 +1,44 @@
-// import { useNavigate } from "react-router-dom";
-
-import { getMenu, getOrder, deleteOrder } from "../../lib/RequestHandler";
-import React, { useEffect, useState } from "react";
-
-import CardsMenu from "./CardsMenu";
-import "../../css/Menu.css";
+import React, { useState } from "react";
 import Header from "../Header";
-import AsideMenu from "./asideMenu";
-import { DateOrder } from "./DateOrder";
-import AsideOrders from "./AsideOrders";
-
-export default function Menu() {
-  const [products, setProducts] = useState({});
-  const [typeMenu, setTypeMenu] = useState("");
-  const [orderMenu, setOrder] = useState({});
+import Search from "../Search";
+import Menu from "./Menu";
+import ordersComanda from "./Menu";
+import { getOrder } from "../../lib/RequestHandler";
+import OrdersDone from "./OrdersDone";
+const MenuView = () => {
+  const [changeButtonsView, setChangeButtonView] = useState(false);
   const [changeView, setChangeView] = useState(true);
   const [comandasOrders, setComandasOrders] = useState([]);
-  const [isDrawerOpenOrder, setIsDrawerOpenOrder] = useState(false);
-  const[contador,setContador]=useState(0)
-  const[selectedOder, setSelectedOrder] = useState({});
-  const [total, setTotal] = useState("");
-  const [watchBton, setWatchBton] = useState(true);
 
-  useEffect(() => {
-    typesProducts();
-  }, []);
-
-  const typesProducts = async () => {
-    const products = await getMenu();
-    let productsByTypes = {};
-    for (let i = 0; i < products.length; i++) {
-      let type = products[i].type;
-
-      if (!Object.prototype.hasOwnProperty.call(productsByTypes, type)) {
-        productsByTypes[type] = [];
-      }
-      productsByTypes[type].push(products[i]);
-    }
-    console.log(productsByTypes);
-    setProducts(productsByTypes);
-  };
-
-  const breakfast = () => {
-    setTypeMenu("desayuno");
-    setChangeView(true);
-  };
-  const dinner = () => {
-    setTypeMenu("cena");
-    setChangeView(true);
-  };
   const ordersComanda = async () => {
     const arrayOrders = await getOrder();
-    console.log(arrayOrders);
     setChangeView(false);
-
     setComandasOrders(arrayOrders);
-    console.log(arrayOrders);
     // setOrder(arrayOrders);
   };
-  const handleDelete = async (id) => {
-    const orderDelete = await deleteOrder(id);
-    const arrayOrders = await getOrder();
-    setComandasOrders(arrayOrders);
+
+  let clickButtonHeader = () => {
+    setChangeButtonView(true);
   };
 
   return (
     <main className="menu-container">
-      <Header 
-      updateComandaOrders={ordersComanda}
-      
-       />
-      <section>
-        <section className="search">
-          <img
-            className="Search"
-            alt="searchIcon"
-            src={require("../../assets/Search.png")}
-          />
-          <input type="text" placeholder="Search..."/>
-        </section>
-
-
-  <div className="btnsAndMenu-container">
-    <section className="btnsOfMenu">
-      <button className="breakAndDinner" onClick={breakfast} >Breakfast</button>
-      <button className="breakAndDinner" onClick={dinner}> Dinner</button>
-    </section>
-    <section className="cards-container">
-      
-     { 
-     changeView &&
-     products[typeMenu] &&
-        products[typeMenu].map((product) => {
-          return (
-            <CardsMenu
-              key={product.id}
-              imgProducts={product.image}
-              name={product.name}
-              price={product.price}
-              order={orderMenu}
-              id={product.id}
-              updateOrder={setOrder}
-              contador={contador}
-              setContador={setContador}
-            ></CardsMenu>
-          );
-        })
-      }
-     { 
-     !changeView &&
-        comandasOrders.map((order) => {
-           return (
-            <DateOrder
-              key={order.id}
-              order={order}
-              updateComanda={setComandasOrders}
-              setIsDrawerOpenOrder= {setIsDrawerOpenOrder}
-              setSelectedOrder = {setSelectedOrder}
-              deleteOrder={handleDelete}
-              setTotal={setTotal}
-              setContador={setContador}
-       ></DateOrder>
-        );
-      })
-      }
-    </section>
-    <AsideMenu
-    order={orderMenu} 
-    updateOrder={setOrder}
-    total={total}
-    setTotal={setTotal}
-    contador={contador}
-    
-    />
-    <AsideOrders
-    isDrawerOpenOrder= {isDrawerOpenOrder}
-    setIsDrawerOpenOrder= {setIsDrawerOpenOrder}
-    selectedOder={selectedOder}
-    total={total}
-    setContador={setContador}
-  
-    
-    />
-  </div>
-    </section >
-  </main >
-
+      <Header
+        updateComandaOrders={ordersComanda}
+        updateComandaOrders2={clickButtonHeader}
+      />
+      <Search />
+      {!changeButtonsView && (
+        <Menu
+          changeView={changeView}
+          setChangeView={setChangeView}
+          comandasOrders={comandasOrders}
+          setComandasOrders={setComandasOrders}
+        />
+      )}
+      {changeButtonsView && <OrdersDone />}
+    </main>
   );
-}
+};
+
+export default MenuView;
--- src/routers/PrivateRouters.js
+++ src/routers/PrivateRouters.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import Menu from "../components/Waiters/MenuView";
+import MenuView from "../components/Waiters/MenuView";
 import { auth, onAuthStateChanged } from "../lib/firebase-config";
 import ChefView from "../components/chef/ChefView";
 import Personal from "../components/Admin/PersonalView";
@@ -33,7 +33,7 @@ const PrivateRoutes = () => {
   } else if (role === "waiter") {
     return (
       <Routes>
-        <Route path="/" element={<Menu />} />
+        <Route path="/" element={<MenuView />} />
       </Routes>
     );
   } else if (role === "chef") {
